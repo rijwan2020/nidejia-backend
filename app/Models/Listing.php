ade8e0a7f79fc5e09eda9083cdcac8ca51c696ed
Rijwan <rijwanrpl18@gmail.com>
2025-04-23 21:08:56 +0700

    Listing resource: listing model

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Listing extends Model
 {
@@ -29,4 +30,15 @@ class Listing extends Model
     protected $cast = [
         'attachments' => 'array',
     ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
