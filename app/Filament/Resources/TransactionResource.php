d95098f5c828b92ac9ea51fc5dc62f0df423b906
Rijwan <rijwanrpl18@gmail.com>
2025-04-28 20:21:50 +0700

    Transaction resource - Table adjustment

--- app/Filament/Resources/TransactionResource.php
+++ app/Filament/Resources/TransactionResource.php
@@ -7,8 +7,12 @@ use App\Filament\Resources\TransactionResource\RelationManagers;
 use App\Models\Transaction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Notifications\Notification;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -24,49 +28,15 @@ class TransactionResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-credit-card';
 
-    public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('listing_id')
-                    ->relationship('listing', 'title')
-                    ->required(),
-                Forms\Components\Select::make('user_id')
-                    ->relationship('user', 'name')
-                    ->required(),
-                Forms\Components\DatePicker::make('start_date')
-                    ->required(),
-                Forms\Components\DatePicker::make('end_date')
-                    ->required(),
-                Forms\Components\TextInput::make('price_per_day')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('total_days')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('fee')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('total_price')
-                    ->required()
-                    ->numeric()
-                    ->default(0),
-                Forms\Components\TextInput::make('status')
-                    ->required(),
-            ]);
-    }
-
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('listing.title')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('user.name')
+                    ->numeric()
+                    ->sortable()
+                    ->weight(FontWeight::Bold),
+                Tables\Columns\TextColumn::make('listing.title')
                     ->numeric()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('start_date')
@@ -75,33 +45,38 @@ class TransactionResource extends Resource
                 Tables\Columns\TextColumn::make('end_date')
                     ->date()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('price_per_day')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('total_days')
                     ->numeric()
                     ->sortable(),
-                Tables\Columns\TextColumn::make('fee')
-                    ->numeric()
-                    ->sortable(),
                 Tables\Columns\TextColumn::make('total_price')
-                    ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('status'),
-                Tables\Columns\TextColumn::make('created_at')
-                    ->dateTime()
+                    ->money('USD')
                     ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
-                Tables\Columns\TextColumn::make('updated_at')
-                    ->dateTime()
-                    ->sortable()
-                    ->toggleable(isToggledHiddenByDefault: true),
+                    ->weight(FontWeight::Bold),
+                Tables\Columns\TextColumn::make('status')->badge()->color(fn (string $state): string => match ($state) {
+                    'waiting' => 'warning',
+                    'approved' => 'success',
+                    'cancelled' => 'danger',
+                }),
             ])
             ->filters([
-                //
+                SelectFilter::make('status')->options([
+                    'waiting' => 'Waiting',
+                    'approved' => 'Approved',
+                    'cancelled' => 'Cancelled',
+                ])
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Action::make('approve')
+                    ->button()
+                    ->color('success')
+                    ->requiresConfirmation()
+                    ->action(function (Transaction $transaction) {
+                        Transaction::find($transaction->id)->update([
+                            'status' => 'approved',
+                        ]);
+                        Notification::make()->success()->title('Transaction Approved')->body('The transaction has been approved')->icon('heroicon-o-check')->send();
+                    })
+                    ->hidden(fn (Transaction $transaction): bool => $transaction->status !== 'waiting'),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
