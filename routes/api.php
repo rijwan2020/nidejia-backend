f34326366aab0f358fed0ca8c91ece02a0c876a7
Rijwan <7rijwan1998@gmail.com>
2025-04-29 19:26:08 +0700

    Auth Endpoint - Me Endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json([
+        'success' => true,
+        'data' => $request->user(),
+    ]);
 });
 
 require __DIR__.'/auth.php';
