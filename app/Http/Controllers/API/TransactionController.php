9e2ec15d76dab821b0a9ee811f0f5922837d0aa3
Rijwan <7rijwan1998@gmail.com>
2025-04-30 21:02:31 +0700

    Transaction Endpoint - Fully Booked Checker

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Transaction\Store;
+use App\Models\Listing;
+use App\Models\Transaction;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    private function _fullyBookedChecker(Store $request)
+    {
+        $listing = Listing::find($request->listing_id);
+        $runningTransactionCount = Transaction::whereListingId($listing->id)
+            ->whereNot('status', 'cancelled')
+            ->where(function ($query) use ($request) {
+                $query->whereBetween('start_date', [
+                    $request->start_date,
+                    $request->end_date
+                ])->orWhereBetween('end_date', [
+                    $request->start_date,
+                    $request->end_date
+                ])->orWhere(function ($subquery) use ($request) {
+                    $subquery->where('start_date', '<', $request->start_date)
+                        ->where('end_date', '>', $request->end_date);
+                });
+            });
+        if ($runningTransactionCount > $listing->max_person) {
+            throw new HttpResponseException(
+                response()->json([
+                    'success' => false,
+                    'message' => 'Listing is fully booked',
+                ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+            );
+        }
+
+        return true;
+    }
+}
